d9f94bc6d5827e23633be6f13b66ba1bf60c4569
jmatis <jan.matis@gmail.com>
2016-12-07 19:56:30 +0000

    Update watchlog.ps1

--- watchlog.ps1
+++ watchlog.ps1
@@ -1,30 +1,38 @@
 Param(
+  # log file to monitor
   [Parameter(Mandatory=$True)]
   [string]$logfile,
   
+  # bookmark file (holds last checked line number)
   [Parameter(Mandatory=$True)]
   [string]$bookmarkfile,
   
+  # flag file - all found errors are stored here (delete to clear alert )
   [Parameter(Mandatory=$True)]
   [string]$flagfile,
   
+  # powershell regular multiline expression pattern 
   [Parameter(Mandatory=$True)]
   [string]$pattern
 )
 
+
 function how_many_lines {
+  # checks how many lines are in logfile
   param ( [string] $logFile )
   $count = 0
   If (Test-Path $logFile){
   Get-Content -Path $logFile -ReadCount 100 |% { $count += $_.Count }
   } else {
     write-host "FAIL! unable to find $logFile"
-	exit 2
+    exit 2
   }
   return ($count)
 }
 
+
 function load_previous_lines{
+  # loads number from file
   param ( [string] $bookmarkfile )
   $lines=0
   If (Test-Path($bookmarkfile)){
@@ -34,31 +42,41 @@ function load_previous_lines{
   return($lines)
 }
 
+
 function save_previous_lines{
+  # saves number to file 
   param ( [string] $bookmarkfile, 
   [int32] $count )
   $count >  $bookmarkfile
 }
 
+# load how many lines we have analized in previous run from bookmarkfile
 $oldlines = load_previous_lines -bookmarkfile $bookmarkfile
+# check how many lines are now in logfile
 $currentlines = how_many_lines -logfile $logfile
 
+# we assume number of lines changed since previous run 
+# possible bug - if file is recreated, and coincidentally has same amount of lines as during previous run we will miss
+# all entries until last line from previous run 
 if ( $currentlines -ne $oldlines) {
+  # current lines bigger than in previous lines (file grew )
   if ( $currentlines -gt $oldlines ){
     $lastlines = $currentlines - $oldlines
-    $found=get-Content -Path $logFile -tail $lastlines 
+    $found=get-Content -Path $logFile -tail $lastlines | out-string
+  # current lines smaller than in previous run (new file )
   } elseif ( $oldlines -gt $currentlines) {
-    $found=get-Content -Path $logFile
+    $found=get-Content -Path $logFile | out-string
   }
-  foreach ($line in $found) {
-    if ( $line | select-string -pattern $pattern -quiet ) {
-	  $line >> $flagfile
-    }
+  
+  if ( $found | select-string -pattern $pattern -quiet ) {
+     $found | select-string -pattern $pattern >> $flagfile
   }
 }
 
+# store number of lines we have analized 
 save_previous_lines -bookmarkfile $bookmarkfile -count $currentlines
 
+# existence of flagfile suggests there were errors found in this(or previous) runs
 If (Test-Path($flagfile)){
   $exitcode=2
   write-host "FAIL: $flagfile exists!"
